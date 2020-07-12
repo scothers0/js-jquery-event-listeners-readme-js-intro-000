54952123708566106260872875b2308ded4067bd
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-07-12 17:22:41 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt(){
+  $("p").click(function(){
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $('img').load(function(){
+    $('img').addClass('tasty');
+  })
+}
+
+function submitIt() {
+  $('form').submit(function(){
+    alert("Your form is going to be submitted now.");
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown' , function(key){
+    if (key.which == 71){
+      alert("g was pressed");
+    }
+  })
+}
 
 $(document).ready(function(){
+  getIt();
+  frameIt();
 
+  submitIt();
+  pressIt();
 // call functions here
 
 });
